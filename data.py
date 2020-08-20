f0477f990b1b8439c1d24c129109a5eea28212fd
ErrorInever <errorinever@gmail.com>
2020-08-20 08:11:59 +0000

    add display

--- data.py
+++ data.py
@@ -3,8 +3,10 @@ from torchvision import transforms, datasets
 
 def get_emnist_dataset():
     compose = transforms.Compose([
+        transforms.RandomRotation(degrees=(-90, -90)),
+        transforms.RandomHorizontalFlip(p=1),
         transforms.ToTensor(),
         transforms.Normalize((.5,), (.5,))
     ])
     out_dir = './dataset'
-    return datasets.EMNIST(root=out_dir, train=True, transforms=compose, download=True)
+    return datasets.EMNIST(root=out_dir, split='bymerge', train=True, transform=compose, download=True)
